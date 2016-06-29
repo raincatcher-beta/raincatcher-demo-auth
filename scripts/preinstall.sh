4079af1cf76b94dfab4d50c806f0a1258fdb7fa5
Leonardo Rossetti <lrossetti@localhost.localdomain>
2016-06-29 16:13:32 -0300

    fh-mbaas-api version update

---
+++ scripts/preinstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+NODE_GYP="$(npm list -g node-gyp | grep node-gyp)"
+NODE_PRE_GYP="$(npm list -g node-pre-gyp | grep node-pre-gyp)"
+
+if ! [ -n "$NODE_PRE_GYP" ]; then
+  npm install node-pre-gyp
+else
+  echo "node-pre-gyp already installed"
+fi
+
+if ! [ -n "$NODE_GYP" ]; then
+  npm install node-gyp
+else
+  echo "node-gyp already installed"
+fi
+
