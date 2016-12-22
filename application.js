939c3993c7845afada9a05b7241e7331bc693794
Paolo Bueno <paolohaji@gmail.com>
2016-12-22 12:31:31 -0200

    Change default storage to redis

--- application.js
+++ application.js
@@ -31,13 +31,11 @@ app.use('/api', bodyParser.json({limit: '10mb'}));
 // Session and Cookie configuration
 // This is being consumed in the raincatcher-user mbaas router.
 var sessionOptions = {
-  store: 'mongo',
+  store: 'redis',
   config: {
     secret: process.env.FH_COOKIE_SECRET || 'raincatcher',
-    host: '127.0.0.1',
-    port: '27017',
-    db: 'session',
-    url: 'mongodb://localhost:27017/raincatcher-demo-auth-session-store',
+    host: process.env.FH_REDIS_HOST || '127.0.0.1',
+    port: process.env.FH_REDIS_PORT || '6379',
     resave: false,
     saveUninitialized: true,
     cookie: {
